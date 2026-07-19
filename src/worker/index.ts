62981d865c468d6328ff64a4000a20fc3f4b99eb
hushin <ngtv.hsn@gmail.com>
2026-07-20 00:24:43 +0900

    oxfmt 導入

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,8 +1,6 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
 
-const app = new Hono<{ Bindings: Env }>().get("/api/", (c) =>
-	c.json({ name: "Cloudflare" }),
-);
+const app = new Hono<{ Bindings: Env }>().get('/api/', (c) => c.json({ name: 'Cloudflare' }));
 
 export default app;
 export type AppType = typeof app;
